90b046ace0c8a1ad2d53dc9d42e03d60dba5109c
Yann Hodique <yann.hodique@gmail.com>
2011-11-21 23:46:27 +0100

    package version 0.1

--- pcache.el
+++ pcache.el
@@ -4,6 +4,7 @@
 
 ;; Author: Yann Hodique <yann.hodique@gmail.com>
 ;; Keywords:
+;; Version: 0.1
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
