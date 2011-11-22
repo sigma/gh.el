3696f1902e3f72b165fecf48e3debcd6d4827774
Yann Hodique <yann.hodique@gmail.com>
2011-11-22 17:23:24 +0100

    v0.2

--- pcache.el
+++ pcache.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yann Hodique <yann.hodique@gmail.com>
 ;; Keywords:
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((eieio "1.4"))
 
 ;; This file is free software; you can redistribute it and/or modify
