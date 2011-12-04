ea51da075865f41db385fb38276cc88fdd43b995
Yann Hodique <yann.hodique@gmail.com>
2011-12-04 19:41:21 +0100

    restructure documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'ghel.tex', u'gh.el Documentation',
+  ('index', 'ghel.tex', u'gh.el User Manual',
    u'Yann Hodique', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'ghel', u'gh.el Documentation',
+    ('index', 'ghel', u'gh.el User Manual',
      [u'Yann Hodique'], 1)
 ]
 
@@ -227,7 +227,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'gh', u'gh.el Documentation',
+  ('index', 'gh', u'gh.el User Manual',
    u'Yann Hodique', 'gh.el', 'Using GitHub from Emacs.',
    'Emacs'),
 ]
