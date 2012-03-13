fc33c2e60b601a13b537796f4cda7c32991008d5
Yann Hodique <yann.hodique@gmail.com>
2012-03-13 21:10:40 +0100

    simple test for gists list

---
+++ tests/gh-gist-tests.el
@@ -0,0 +1,50 @@
+;;; gh-gist-tests.el --- tests for gh-gist.el
+
+;; Copyright (C) 2012  Yann Hodique
+
+;; Author: Yann Hodique <yann.hodique@gmail.com>
+;; Keywords:
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'gh-tests)
+(require 'gh-gist)
+
+(ert-deftest gh-gist-tests-regular ()
+  (let ((gists
+         (gh-tests-with-traces-buffers ((gists-buf "list_gists_sample.txt"))
+           (mocker-let ((url-retrieve-synchronously
+                         (url)
+                         ((:record-cls mocker-stub-record
+                                       :output gists-buf))))
+             (let ((api (gh-gist-api "api" :sync t)))
+               (oref
+                (gh-gist-list api "octocat")
+                :data))))))
+    (should (equal (length gists) 1))
+    (let ((gist (car gists)))
+      (should (equal (oref gist :id) "1"))
+      (should (oref gist :public))
+      (should (equal (length (oref gist :files)) 1)))))
+
+(provide 'gh-gist-tests)
+;;; gh-gist-tests.el ends here
