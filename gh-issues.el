7cd2776ee5831137a3e6fb8cabcb70f0df2c9c71
Raimon Grau <raimonster@gmail.com>
2012-03-16 00:27:19 +0100

    remove duplicated line

--- gh-issues.el
+++ gh-issues.el
@@ -93,7 +93,6 @@
                    closed_issues created_at due_on)
       issue
     (setq url (gh-read data 'url)
-          url (gh-read data 'url)
           html-url (gh-read data 'html-url)
           number (gh-read data 'number)
           state (gh-read data 'state)
