6794b62344004ee68e14a4ab69cbb3f6dd3dd78b
Yann Hodique <hodiquey@vmware.com>
2015-01-25 16:54:16 -0800

    silence compiler

--- gh-cache.el
+++ gh-cache.el
@@ -42,12 +42,14 @@
   (format "%s/gh-%s" pcache-version-constant gh-cache-internal-version-constant))
 
 (defclass gh-cache (pcache-repository)
-  ((version-constant :allocation :class :initform (eval gh-cache-version-constant))
+  ((version-constant :allocation :class)
    (entries :initarg :entries :initform (make-hash-table :test 'equal))
    (safe-methods :allocation :class :initform ("HEAD" "GET" "OPTIONS" "TRACE"))
    (invalidation-chain :allocation :class :initform nil)
 
    (entry-cls :initarg :entry-cls :initform gh-cache-entry)))
+
+(oset-default 'gh-cache version-constant gh-cache-version-constant)
 
 (defclass gh-cache-entry (pcache-entry)
   ((etag :initarg :etag :initform nil)
