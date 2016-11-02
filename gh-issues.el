f0dcd8d323dd5e16f7213eb7fc209e8cee277844
Jingtao Xu <jingtaozf@gmail.com>
2016-11-02 15:01:13 +0800

    add issue field assignees

--- gh-issues.el
+++ gh-issues.el
@@ -25,7 +25,7 @@
 ;; Basic usage:
 
 ;; (setf api (gh-issues-api "api" :sync nil :cache nil :num-retries 1))
-;; (setf issues (gh-issues-list api "user" "repo"))
+;; (setf issues (gh-issues-issue-list api "user" "repo"))
 ;; (last (oref issues data)) ; get one issue
 ;; (setq mi (make-instance 'gh-issues-issue :body "issue body" :title "issue title"))
 ;; (gh-issues-issue-new api "user" "repo" mi)
@@ -69,7 +69,7 @@
    (milestone :initarg :milestone :initform nil :marshal-type gh-issues-milestone)
    (comments :initarg :comments :initform 0)
    (pull-request :initarg :pull-request :marshal-type gh-issues-pull-request)
-   (closed-at :initarg :created-at)
+   (closed-at :initarg :closed-at)
    (created-at :initarg :created-at)
    (updated-at :initarg :updated-at))
   "issues request")
